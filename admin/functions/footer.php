a34bcd0b5e1ccd0d9a57766a28503c0e57dc1e6e
Hossein Gerami <shadow_barsa@yahoo.com>
2017-08-14 16:42:00 +0430

    update

--- admin/functions/footer.php
+++ admin/functions/footer.php
@@ -3,7 +3,7 @@ if ( __FILE__ == $_SERVER['SCRIPT_FILENAME'] ) { exit; }
 
 // Custom Footer
 function aquila_admin_footer_admin () {
-	echo '<a href="https://designbymito.com/" class="mitoLink" target="_blank">design by Mito</a>';
+	echo '<a href="https://websitehouse.ir/" class="mitoLink" target="_blank">طراحی و توسعه توسط وب سایت هاوس</a>';
 }
 add_filter('admin_footer_text', 'aquila_admin_footer_admin');
 
--- login/aquila-login-screen.php
+++ login/aquila-login-screen.php
@@ -6,7 +6,7 @@ function aquila_admin_login_logo_url() {
 add_filter( 'login_headerurl', 'aquila_admin_login_logo_url' );
 
 function aquila_admin_login_logo_title() {
-    return 'Aquila';
+    return 'WebsiteHouse';
 }
 add_filter( 'login_headertitle', 'aquila_admin_login_logo_title' ); 
 
